d9b08200c0dcfd7b51092969a19e5dd82fb8495c
zxf199 <zxf12924@163.com>
2019-02-19 15:50:27 +0800

    解决了vue路由history上线刷新是访问不到对应的路由

--- servers/index.js
+++ servers/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const history = require('connect-history-api-fallback') // 为了vue路由的history模式配置的中间件
+app.use(history()) // 必须放在static上面 nginx.conf在服务器上查找的路径是cd /usr/local/nginx/conf
 app.use(express.static(path.join(__dirname, '../dist')))
 app.listen(3001, function () {
-  console.log('监听3000端口成功')
+  console.log('监听3001端口成功')
 })
--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Login from '@/components/login'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
