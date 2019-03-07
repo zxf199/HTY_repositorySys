211ea620d51c297e61a5f0a722e91577da4e39ba
zxf199 <zxf12924@163.com>
2019-03-07 10:05:49 +0800

    删除

--- src/components/parts/partsInput/partsInputAudit.vue
+++ src/components/parts/partsInput/partsInputAudit.vue
@@ -1,14 +1,11 @@
 <template>
   <div>
     这是入库申请界面
-    <button @click="getInfo">获取</button>
+    <button>获取</button>
   </div>
 </template>
 <script>
 export default {
-  name: 'partsInputAudit',
-  methods: {
-    getInfo () {}
-  }
+  name: 'partsInputAudit'
 }
 </script>
